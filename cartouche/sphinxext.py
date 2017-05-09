f68e4b51aced7bf46f27e3c14e4ec21a15252cd5
Stian Lode <stian.lode@gmail.com>
2017-05-09 10:03:29 +0200

    Adds flags to setup() to allow sphinx to read and write in parallel.
    Sphinx 1.5+ requires a few flags to be set to allow parallel reading and writing. This has a significant effect on overall build time.

--- cartouche/sphinxext.py
+++ cartouche/sphinxext.py
@@ -1,4 +1,5 @@
 from .parser import (rewrite_autodoc, builder_inited)
+from .version import (__version__)
 
 __author__ = 'Robert Smallshire'
 
@@ -9,3 +10,9 @@ def setup(app):
     app.add_config_value('cartouche_accept_bulleted_raises', False, 'env')
     app.connect('builder-inited', builder_inited)
     app.connect('autodoc-process-docstring', rewrite_autodoc)
+
+    return dict(
+        version = __version__,
+        parallel_read_safe = True,
+        parallel_write_safe = True
+    )
